be904af1b3e3a42a5a095abe6e75fcef5d8541f3
Dmitriq93 <topnikov_93@mail.ru>
2023-11-06 22:30:23 +0300

    Задание №7_Unit тесты

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -11,34 +11,32 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculatorTest {
     private Calculator calculator = new Calculator();
-    private CalculatorApp calculatorApp =new CalculatorApp();
-//    private  CalculatorApp calculatorApp = new CalculatorApp();
-//
-//    @Test
-//    public void addsTwoNumbers() {
-//        assertEquals(8.0, calculator.makeCalculation(6.0, 2.0, '+'), 1);
-//    }
-//
-//    @Test
-//    public void subtractsTwoNumbers() {
-//        assertEquals(4.00, calculator.makeCalculation(6.0, 2.0, '-'), 1);
-//    }
-//
-//    @Test
-//    public void multipliesTwoNumbers() {
-//        assertEquals(12.0, calculator.makeCalculation(6.0, 2.0, '*'), 1);
-//    }
-//
-//    @Test
-//    public void dividesTwoNumbers() {
-//        assertEquals(3.0, calculator.makeCalculation(6.0, 2.0, '/'), 1);
-//
-//    }
+
+    @Test
+    public void addsTwoNumbers() {
+        assertEquals("8.0", calculator.makeCalculation(6.0, 2.0, '+'));
+    }
+
+    @Test
+    public void subtractsTwoNumbers() {
+        assertEquals("4.0", calculator.makeCalculation(6.0, 2.0, '-'));
+    }
+
+    @Test
+    public void multipliesTwoNumbers() {
+        assertEquals("12.0", calculator.makeCalculation(6.0, 2.0, '*'));
+    }
+
+    @Test
+    public void dividesTwoNumbers() {
+        assertEquals("3.0", calculator.makeCalculation(6.0, 2.0, '/'));
+
+    }
 
     @Test
     void dividesNullArithmeticException() {
         Assertions.assertThrows(AssertionFailedError.class, () -> {
-            assertEquals(3.0, calculator.makeCalculation(6.0, 0, '/'));
+            assertEquals("3.0", calculator.makeCalculation(6.0, 0, '/'));
         });
     }
 
